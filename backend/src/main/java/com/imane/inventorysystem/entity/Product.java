eb1cbe0e4ad44f4861ead20fc8045fe5a0bc6f93
ImaneElla <emanellaouzi.05@gmail.com>
2026-04-21 23:54:39 +0100

    feat: create initial Product entity and repository

---
+++ backend/src/main/java/com/imane/inventorysystem/entity/Product.java
@@ -0,0 +1,5 @@
+package com.imane.inventorysystem.entity;
+
+public class Product {
+    
+}
---
+++ backend/src/main/java/com/imane/inventorysystem/repository/ProductRepository.java
@@ -0,0 +1,5 @@
+package com.imane.inventorysystem.repository;
+
+public class ProductRepository {
+    
+}
